4d7c01205a64201c97cc9cf00597f0e7a8ea46a8
Heisenberg-kd <kartikaya2000@gmail.com>
2024-03-19 17:54:16 +0530

    Minor Patch

--- Final_Project/Controller/opt_problem.m
+++ Final_Project/Controller/opt_problem.m
@@ -2,7 +2,6 @@ function Uk = opt_problem(P0,P1,P2,q1,q2,r1,r2,d)
   fun= @(Uk)Uk(:)'*P0*Uk(:);
   nonlcon = @(Uk) constraint(Uk(:), P1, P2, q1, q2, r1, r2,d);
   Uk = fmincon(fun,[5,5,9],[],[],[],[],[],[],nonlcon);
-
 end
 
 function [c,ceq] = constraint(Uk,P1,P2,q1,q2,r1,r2,d)
